946396d9b45eefd8b4c41eb05f4f69f153dcf58f
Matthew (mandatory) Bryant <mbryant2@snapchat.com>
2021-08-19 11:03:10 -0700

    ok

--- client-update.rb
+++ client-update.rb
@@ -1,6 +1,8 @@
 class ClientUpdate < Formula
   desc "This is a test formula"
   homepage "https://example.com"
+  url "https://github.com/onetrueawk/awk/archive/20180827.tar.gz"
+  sha256 "c9232d23410c715234d0c26131a43ae6087462e999a61f038f1790598ce4807f"
   license "GPL-2.0-or-later"
 
   def install
